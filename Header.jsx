6f3b3144b3a1e240e942cc81f9414647aac4e646
avanthika-23 <80565236+avanthika-23@users.noreply.github.com>
2021-06-06 18:36:06 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Web Dev from SHAPEAI</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        I attended this bootcamp conducted by SHAPEAI . The topic is Web
+        development with Javascript and React.js. Our instructor was Shaurya
+        Sinha. This is my first project in web development
+      </p>
+    </div>
+  );
+}
+
+export default Note;
